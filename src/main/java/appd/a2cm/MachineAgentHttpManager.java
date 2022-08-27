08944c769f0f76b32652d6e44ce304f178c53daf
Anwar Fazza <anwar_fazza@live.com>
2022-08-27 03:56:45 +0300

    Disabled Logger

--- src/main/java/appd/a2cm/MachineAgentHttpManager.java
+++ src/main/java/appd/a2cm/MachineAgentHttpManager.java
@@ -11,24 +11,31 @@ import org.apache.http.HttpResponse;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import org.apache.log4j.Logger;
 
 public class MachineAgentHttpManager {
+    
+    static Logger log = Logger.getLogger(MachineAgentHttpManager.class.getName());
+    
     private HttpClient httpClient;
 
     private HttpPost request;
 
     public MachineAgentHttpManager(String MachineAgentIpAddress,int MachineAgentPort) throws IOException {
+        
         this.httpClient = HttpClientBuilder.create().build();
         this.request = new HttpPost("http://" + MachineAgentIpAddress + ":" + MachineAgentPort + "/api/v1/metrics");
+        log.debug("Sent request: "+ request.getURI());
     }
 
-    public void postData(String metricPath, String value) throws UnsupportedEncodingException {
-       StringEntity parameters = new StringEntity("[{\"metricName\":\"" + metricPath + "\",\"aggregatorType\":\"AVERAGE\",\"value\":" + value + "}]");
-        try {
+    public void postData(String metricPath, String value) {
+      try {
+        StringEntity parameters = new StringEntity("[{\"metricName\":\"" + metricPath + "\",\"aggregatorType\":\"AVERAGE\",\"value\":" + value + "}]");
+        
             this.request.setEntity(parameters);
             HttpResponse response = this.httpClient.execute(this.request);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (Exception ex) {
+            log.error("Post data exception: " + ex);
         }
     }
 }
